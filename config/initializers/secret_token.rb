e9e1e64f14375e89000d4bbd26594a17026b6ff2
hadwinzhy <hadwinzhy@gmail.com>
2013-09-01 12:59:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoctorJS::Application.config.secret_token = '6406da0a986fc52fe5aed6cba6301851f617728bc4c81a0fe2687cd4bfe7dab6c09cee68b94c8ef516bfbe4ed7293091de6ac63041b17897c5531542e7588f34'
